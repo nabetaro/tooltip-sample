55ed8c8cf8888d40a338753fab6dbdfa75d84442
KURASAWA Nozomu <nabetaro@caldron.jp>
2014-02-12 07:37:15 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TooltipSample::Application.config.secret_token = 'ffa83d7f24f972fc14b3d9d0c7ee0a8d0e573ccde767f057eb6a87daaab395d100805bae3b5d2a45d5fb2a6db614c85a0943fdbb4c7bf3f1fd6d6c6c5a478f54'
